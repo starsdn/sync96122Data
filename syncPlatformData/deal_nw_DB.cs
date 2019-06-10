8026c2e152fe8a346937433fc723833ad71c2c86
Xusl000 <43329062+Xusl000@users.noreply.github.com>
2019-06-10 17:32:14 +0800

    xsl

--- syncPlatformData/deal_nw_DB.cs
+++ syncPlatformData/deal_nw_DB.cs
@@ -104,6 +104,7 @@ namespace syncPlatformData
 
                     string SourceID = dr["SOURCEID"].ToString();//数据来源ID
                     string mylyguid = string.Empty;//二期民意来源guid
+                    int upReportMyly = 0;//上报民意来源对应number
                     switch (SourceID)
                     {
                         //6062178399694A078E1AACF5FDD9F481 寒山闻钟论坛----------支队寒山闻钟
@@ -116,30 +117,32 @@ namespace syncPlatformData
                         //BED2CF750A634A9D876262BB670A3A39 支队信件转办----------支队
                         //0C51C38CFF464CB4B839FA663C181CDE 其他自选项------------分局寒山闻钟、分局政风行风
 
+                        // 1寒山闻钟论坛；2：12345热线；3纪委条线；4：信访条线；5省厅、总队信件办理；6督察条线来信；7支队信件转办；8其他自选项；9：96122
+
                         case "1":   //支队寒山闻钟
-                            mylyguid = "6062178399694A078E1AACF5FDD9F481"; break;
+                            mylyguid = "6062178399694A078E1AACF5FDD9F481"; upReportMyly = 1; break;
                         case "2":   //支队政风行风
-                            mylyguid = "0E8E2D480578471EAB622AC8DDC2A874"; break;
+                            mylyguid = "0E8E2D480578471EAB622AC8DDC2A874"; upReportMyly = 2; break;
                         case "3":   //96122工单
-                            mylyguid = "510521AAF5FC4F06BD58C2696BDA22E5"; break;
+                            mylyguid = "510521AAF5FC4F06BD58C2696BDA22E5"; upReportMyly = 9; break;
                         case "5":   //分局寒山闻钟
-                            mylyguid = "0C51C38CFF464CB4B839FA663C181CDE"; break;
+                            mylyguid = "0C51C38CFF464CB4B839FA663C181CDE"; upReportMyly = 8; break;
                         case "6":   //分局政风行风
-                            mylyguid = "0C51C38CFF464CB4B839FA663C181CDE"; break;
+                            mylyguid = "0C51C38CFF464CB4B839FA663C181CDE"; upReportMyly = 8; break;
                         case "7":   //市局信访处
-                            mylyguid = "58EAE7497C054DBCB3241A15C9336795"; break;
+                            mylyguid = "58EAE7497C054DBCB3241A15C9336795"; upReportMyly = 4; break;
                         case "8":   //市局纪委
-                            mylyguid = "7B8F5B74C09243FA9E2B5540B708761D"; break;
+                            mylyguid = "7B8F5B74C09243FA9E2B5540B708761D"; upReportMyly = 3; break;
                         case "9":   //1号窗口
                             mylyguid = ""; break;
                         case "10":   //市局平安民声
                             mylyguid = ""; break;
                         case "11":   //支队
-                            mylyguid = "BED2CF750A634A9D876262BB670A3A39"; break;
+                            mylyguid = "BED2CF750A634A9D876262BB670A3A39"; upReportMyly = 7; break;
                         case "12":   //市局督查
-                            mylyguid = "09B7732CDBEA4009A1FA9DC5166B7690"; break;
+                            mylyguid = "09B7732CDBEA4009A1FA9DC5166B7690"; upReportMyly = 6; break;
                         case "13":   //其他
-                            mylyguid = "EA6859A55E7A44289A6B589D887C61AC"; break;
+                            mylyguid = "EA6859A55E7A44289A6B589D887C61AC"; upReportMyly = 5; break;
                         default: break;
                     }
 
@@ -155,14 +158,14 @@ namespace syncPlatformData
                     Model.ASSIGN model_assign = new Model.ASSIGN(); //ASSIGN 主表
                     model_assign.BZ = "";//备注空
                     model_assign.CJR = "民意一期";//创建人
-                    model_assign.CJSJ = DateTime.Now;//创建时间
-                    model_assign.HFNR = "";//各部门回复内容汇总
+                    model_assign.CJSJ = Convert.ToDateTime(dr["CREATE_DATE"].ToString());//创建时间
+                    //model_assign.HFNR = "";//各部门回复内容汇总
                     model_assign.ID = assign_guid;
                     model_assign.JBDW = "";//交办单位
-                    model_assign.JBRQ = Convert.ToDateTime(dr["assigned_date"] + ""); //交办日期
-                    model_assign.JBYQ = "";//交办要求
-                    model_assign.LDPS = "";//领导批示
-                    model_assign.MYLY = mylyguid;//96122
+                    model_assign.JBRQ = Convert.ToDateTime(dr["ASSIGNED_DATE"].ToString()); //交办日期
+                    //model_assign.JBYQ = "";//交办要求
+                    //model_assign.LDPS = "";//领导批示
+                    model_assign.MYLY = mylyguid;
                     model_assign.MYSJDX = con_guid;//民意数据来源
                     model_assign.ZT = 5; //已办结
                     #endregion
@@ -172,30 +175,43 @@ namespace syncPlatformData
                     Model.INWORK model_inwork = new Model.INWORK();//处办表
                     model_inwork.ID = inwork_guid;
                     model_inwork.CJR = "民意一期";//创建人
-                    model_inwork.CJSJ = DateTime.Now;//创建时间
+                    model_inwork.CJSJ = Convert.ToDateTime(dr["CREATE_DATE"].ToString());//创建时间
                     model_inwork.ISTX = 0;
                     model_inwork.ISZB = 2;//1 是 2否  是否转办
                     model_inwork.JBDW = "";//交办单位
                     model_inwork.JBSJ = assign_guid;//交办数据
+                    model_inwork.CBLX = 1;//出版类型 1办理 2拒绝
                     model_inwork.ZT = 6; //状态 已办结
                     #endregion
 
                     #region 添加部门答复数据
                     Model.REPLY_RECORD model_reply = new Model.REPLY_RECORD();
                     model_reply.ID = Guid.NewGuid().ToString("N").ToUpper(); //得到32位大写的 GUID
-
                     model_reply.CBID = inwork_guid;//处办id
                     model_reply.HFNR = dr["REPLY_CONTENT"].ToString();//回复内容
                     model_reply.ZT = 1;//状态
                     model_reply.CJR = "民意一期";//创建人
-                    model_reply.CJSJ = DateTime.Now;//创建时间
+                    model_reply.CJSJ = Convert.ToDateTime(dr["CREATE_DATE"].ToString());//创建时间
                     model_reply.CBLX = 1;//处办类型
-                    model_reply.FJDZ = "";//附件地址
+                    model_reply.FJDZ =dr["REPLY_CONTENTFILE"].ToString();//附件地址
                     model_reply.FJM = "";//附件名
                     #endregion
 
                     #region 添加上报数据
+                    string fylx_MY1 = dr["TYPES"].ToString();//民意一期反映类型
+                    string fyle_MY2 = string.Empty;//民意二期反映类型
+                    if (dicFYLXID_CODE.ContainsKey(fylx_MY1))
+                    {
+                        fyle_MY2 = dicFYLXID_CODE[fylx_MY1]; //得到字典中对应的ID
+                    }
                     Model.ASSIGN_UPREPORT model_upreport = new Model.ASSIGN_UPREPORT();
+                    model_upreport.ID = Guid.NewGuid().ToString("N").ToUpper(); //得到32位大写的 GUID
+                    model_upreport.ASSIGN_ID = assign_guid;//交办id
+                    model_upreport.DEALCONTENT = dr["REPLY_CONTENT"].ToString(); //调查内容
+                    model_upreport.FYLX = fyle_MY2;//反映类型
+                    model_upreport.CREATETIME = Convert.ToDateTime(dr["CREATE_DATE"].ToString());//创建时间
+                    model_upreport.CREATOR = "民意一期";
+                    model_upreport.MYLY = upReportMyly;//民意来源 1寒山闻钟论坛；2：12345热线；3纪委条线；4：信访条线；5省厅、总队信件办理；6督察条线来信；7支队信件转办；8其他自选项；9：96122
                     #endregion
 
                     #region 添加上报附件数据
@@ -210,25 +226,25 @@ namespace syncPlatformData
                     try
                     {
                         //更新最大ID
-                        using (FileStream fs = new FileStream("configs/maxid.txt", FileMode.Open, FileAccess.ReadWrite))
-                        {
-                            using (StreamWriter sw = new StreamWriter(fs, Encoding.GetEncoding("UTF-8")))
-                            {
-                                sw.WriteLine(dr["INFOID"] + "");
-                                sw.Close();
-                                fs.Close();
-                                // Class1 cl = new Class1();
-                                event_showLogs("成功更新configs/maxid.txt最大ID：" + dr["INFOID"]);
-                                SysLog.WriteOptDisk("成功更新configs/maxid.txt最大ID：" + dr["INFOID"], AppDomain.CurrentDomain.BaseDirectory, 100);
-                            }
-                        }
+                        //using (FileStream fs = new FileStream("configs/maxid.txt", FileMode.Open, FileAccess.ReadWrite))
+                        //{
+                        //    using (StreamWriter sw = new StreamWriter(fs, Encoding.GetEncoding("UTF-8")))
+                        //    {
+                        //        sw.WriteLine(dr["INFOID"] + "");
+                        //        sw.Close();
+                        //        fs.Close();
+                        //        // Class1 cl = new Class1();
+                        //        event_showLogs("成功更新configs/maxid.txt最大ID：" + dr["INFOID"]);
+                        //        SysLog.WriteOptDisk("成功更新configs/maxid.txt最大ID：" + dr["INFOID"], AppDomain.CurrentDomain.BaseDirectory, 100);
+                        //    }
+                        //}
                     }
                     catch
                     { }
                     #endregion
                 }
             }
-            catch
+            catch(Exception e)
             {
 
             }
